498b34c182777d2096ee866d7225b4bd097c74dc
hakamir <rodolphe.latour@gmail.com>
2019-06-04 16:24:00 +0200

    Try to modify the yolov3 script under ROS to work. WIP.

--- README.md
+++ README.md
@@ -1,9 +1,41 @@
-# process 3D position
+# Process 3D position
 
 This is a ROS package used to put detected objects thanks to Convolutional Neural Network (Yolov3 in our case) and distance estimator using disparity (Real-time Self-adaptative Deep Stereo of MADNet in our project) into a 3D space. 
 
 Here's the ROS architecture :
 
 ![alt text](https://github.com/hakamir/process_3D_position/blob/master/ROS_diagram_full_2.png)
+
+We use different project from GitHub that are listed below:
+
+### For the object detection: 
+https://github.com/ultralytics/yolov3
+[![DOI](https://zenodo.org/record/2672652#.XPZ-IxixX8s)](http://dx.doi.org/10.5281/zenodo.2675652)
+
+### For the distance estimation: 
+https://github.com/CVLAB-Unibo/Real-time-self-adaptive-deep-stereo
+@InProceedings{Tonioni_2019_CVPR,
+    author = {Tonioni, Alessio and Tosi, Fabio and Poggi, Matteo and Mattoccia, Stefano and Di Stefano, Luigi},
+    title = {Real-time self-adaptive deep stereo},
+    booktitle = {The IEEE Conference on Computer Vision and Pattern Recognition (CVPR)},
+    month = {June},
+    year = {2019}
+}
+
+### To use D435 and T265 cameras from Intel(R) in ROS:
+https://github.com/IntelRealSense/realsense-ros
+
+### To calibrate the data:
+https://github.com/abewley/sort
+@inproceedings{Bewley2016_sort,
+  author={Bewley, Alex and Ge, Zongyuan and Ott, Lionel and Ramos, Fabio and Upcroft, Ben},
+  booktitle={2016 IEEE International Conference on Image Processing (ICIP)},
+  title={Simple online and realtime tracking},
+  year={2016},
+  pages={3464-3468},
+  keywords={Benchmark testing;Complexity theory;Detectors;Kalman filters;Target tracking;Visualization;Computer Vision;Data Association;Detection;Multiple Object Tracking},
+  doi={10.1109/ICIP.2016.7533003}
+}
+
 
 ## *Work in progress*
--- yolo_madnet/scripts/run.py
+++ yolo_madnet/scripts/run.py
@@ -2,6 +2,8 @@
 
 import rospy
 from sensor_msgs.msg import Image
+from yolo_madnet.msg import BboxMsg
+from cv_bridge import CvBridge, CvBridgeError
 
 import sys
 try:
@@ -12,7 +14,7 @@ except ImportError:
     sys.path.append('/opt/ros/kinetic/lib/python2.7/dist-packages')
 
 from yolov3.models import *
-from utils.utils import *
+from yolov3.utils.utils import *
 
 
 class detection:
@@ -29,24 +31,30 @@ class detection:
         cfg = 'yolov3/cfg/yolov3.cfg'
         weights = 'yolov3/weights/yolov3.weights'
         data_cfg = 'yolov3/data/coco.data'
+        class_names = 'yolov3/data/coco.names'
 
         # Load model and weights
-        self.model = Darknet(cfg, img_size)
-        _ = load_darknet_weights(model, weights)
-
+        print('Loading models...')
+        try:
+            self.model = Darknet(cfg, img_size)
+            _ = load_darknet_weights(self.model, weights)
+        except:
+            raise('ERROR Cannot load models. Segmentation fault (core dumped)')
         # Fuse Conv2d + BatchNorm2d layers
         self.model.fuse()
 
         # Eval mode
+        device = torch_utils.select_device()
         self.model.to(device).eval()
-
         # Get classes and colors
-        classes = load_classes(parse_data_cfg(data_cfg)['names'])
+        classes = load_classes(class_names)
+        #classes = load_classes(data_cfg)
         colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(classes))]
 
         rospy.init_node('detection_node')
-        sub_l = rospy.Subscriber('/camera/infrared/left', Image, self.process, queue_size=10)
+        sub_l = rospy.Subscriber('/camera/infra1/image_rect_raw', Image, self.process, queue_size=10)
         pub = rospy.Publisher('/detection', BboxMsg, queue_size=10)
+        rospy.spin()
 
     """
     ROS process. This function perform object detection with Yolov3 working
@@ -56,7 +64,10 @@ class detection:
     """
     def process(self, msg):
         # Input image from topic
-        img = cv2.imread(msg)
+        try:
+            img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        except CvBridgeError as e:
+            print(e)
         img = torch.from_numpy(img).unsqueeze(0).to(device)
         pred, _ = model(img)
         det = non_max_suppression(pred, conf_thres, nms_thres)[0]
