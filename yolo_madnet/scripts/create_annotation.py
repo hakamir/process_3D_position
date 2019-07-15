b17f3d97907bd1d99b6621f1ec2b4390dfc31d67
hakamir <rodolphe.latour@gmail.com>
2019-07-15 14:26:21 +0200

    Final annotation scripts

--- yolo_madnet/scripts/create_annotation.py
+++ yolo_madnet/scripts/create_annotation.py
@@ -10,6 +10,7 @@ class create_annotation:
         rospy.init_node('create_annotation')
         self.bridge = CvBridge()
         object = rospy.Subscriber('/detection/image', DetectionMsg, self.process, queue_size=10)
+        self.i = 0
         rospy.spin()
 
     def process(self, msg):
@@ -22,12 +23,40 @@ class create_annotation:
         except CvBridgeError as e:
             print(e)
         # We perform on every detected objects on the frame
-        print('frame1')
+        size = img.shape
+        img_name = 'img_%08d.jpg'%self.i
+        txt_name = 'img_%08d.txt'%self.i
+        f = open('annotation/' + txt_name, 'a')
         for object in msg.bbox:
-            x1,y1,x2,y2 = object.x1, object.y1, object.x2, object.y2
-            print((x1,y1), (x2,y2))
-
+            bbox = object.x1, object.y1, object.x2, object.y2
+            x, y, w, h = self.convert(size, bbox)
+            print(object.obj_class)
+            if object.obj_class == 'door':
+                _class = 0
+            elif object.obj_class == 'handle':
+                _class = 1
+            elif object.obj_class == 'elevator':
+                _class = 2
+            elif object.obj_class == 'switch':
+                _class = 3
+            print(_class, x, y, w, h)
+            f.write('{} {} {} {} {}\n'.format(_class, x, y, w, h))
+        cv2.imwrite('annotation/' + img_name, img)
+        self.i += 1
         return
+
+    def convert(self,size, box):
+        dw = 1./size[1]
+        dh = 1./size[0]
+        x = (box[0] + box[2])/2.0
+        y = (box[1] + box[3])/2.0
+        w = box[2] - box[0]
+        h = box[3] - box[1]
+        x = x*dw
+        w = w*dw
+        y = y*dh
+        h = h*dh
+        return (x,y,w,h)
 
 if __name__ == '__main__':
     try:
